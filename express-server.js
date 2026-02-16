adbdc5923c3c2f4ddd4dba165703663e13100d4c
greatkevingkpro-svg <greatkevingkpro@gmail.com>
2026-02-16 21:58:04 +0300

    express.js backend

---
+++ express-server.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const { Sale } = require("./sale.js");
+
+const sale = new Sale();
+const app = express();
+
+// middleware
+app.use(express.json());
+
+// get request to the homepage
+app.get("/", (req, res) => {
+  res.send('hello world from from Kevin');
+});
+
+// get request to the sales page
+app.get("/sales", (req, res) => {
+  res.json(sale.getAllSales());
+});
+
+app.get("/sales/:item", (req, res) => {
+  let item = req.params.item;
+
+  let _sale = sale.getSaleByItem(item);
+  console.log(item, _sale)
+  if(_sale) {
+    res.status(200).json(_sale);
+  } else {
+    res.status(404).json({message: "Sale not found"})
+  }
+})
+
+// post request to the sales page
+app.post("/sales", async (req, res) => {
+  let body = req.body;
+  let saveState = await sale.add(body);
+  if(saveState === true) {
+    res.status(201);
+    res.json({message: "sale saved successfully"});
+  } else {
+    res.status(400);
+    res.json({message: "sale could not be saved"});
+  }
+
+  /*
+  try {
+    let body = req.body;
+    let saveState = await sale.add(body);
+
+    if (saveState === true) {
+      return res.status(201).json({
+        message: "sale saved successfully"
+      });
+    } else {
+      return res.status(400).json({
+        message: "sale could not be saved"
+      });
+    }
+
+  } catch (error) {
+    return res.status(500).json({
+      message: "Internal Server Error",
+      error: error.message
+    });
+  }
+  */
+});
+
+app.listen(3000, (err) => {
+  if(err) {
+    console.log('Errror');
+  } else {
+    console.log('Server is successfully running on port 3000');
+  }
+});
---
+++ sale.js
@@ -0,0 +1,49 @@
+const fs = require("fs/promises");
+
+class Sale {
+  constructor(path="sales.json") {
+    this.path = path;
+    this.sales = [];
+
+    fs.readFile(this.path)
+      .then((data, err) => {
+        if(err) {
+          console.log(err);
+        } else {
+          this.sales = JSON.parse(data);
+          // console.log(this.sales);
+        }
+      })
+  }
+  // method to get all sales
+  getAllSales() {
+    return this.sales
+  }
+
+  // get sales by item
+  getSaleByItem(item) {
+    return this.sales.find(sale => sale.item === item);
+  }
+
+  // add a new sale
+  async add(sale) {
+    this.sales.push(sale);
+    return await this.save();
+  }
+
+  async save() {
+    let isSavedSuccessful
+    await fs.writeFile(this.path, JSON.stringify(this.sales))
+      .then(err => {
+        if(err) {
+          isSavedSuccessful = false;
+        } else { 
+          isSavedSuccessful = true;
+        }
+      })
+
+    return isSavedSuccessful;
+  }
+}
+
+module.exports = {Sale};
